f41b3870903c4623e5a94a2b982ea07678c10e81
jhovin <jhovin.bonifacio@tecsup.edu.pe>
2020-07-07 12:51:59 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular14';
+
+  title = 'Heroes';
+  heroes:any[]= [];
+  canFly = true;
+  mutate = true;
+  title2 = "Héroes que vuelan";
+  title3 = "Todos los héroes";
+  reset(){this.heroes=[]}
+  constructor(){
+    this.heroes=[];
+  }
+  addHero(name: String){
+    name = name.trim();
+    if(!name){return;}
+    let hero = {name, canFly: this.canFly};
+    this.heroes = this.heroes.concat(hero);
+  }
 }
